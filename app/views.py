1c405d7cbb3cb1619f3487ee5c4575c8bf18c1cf
sseoy <98001270+01seoyoung@users.noreply.github.com>
2023-08-17 19:39:33 +0900

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -27,12 +27,22 @@ def file_upload(request):
 def result(request):
     return render(request, 'result.html')
 
+def result(request):
+    return render(request, 'home.html')
+from django.shortcuts import render
 
+def result(request):
+    # 등기부등본 분석 및 결과 데이터 생성 로직
+    # 예시 데이터, 실제 데이터에 맞게 수정 필요
+    visualization_image_path = 'images/visualization.png'
+    clustering_result = '클러스터링 결과 및 해석'
+    probability = '확률 정보'
 
-
-
-
-
+    return render(request, 'result.html', {
+        'visualization_image_path': visualization_image_path,
+        'clustering_result': clustering_result,
+        'probability': probability
+    })
 
 
 
